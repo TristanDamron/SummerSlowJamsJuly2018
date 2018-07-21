2b7a420175841873ccfb42f3805d1ee1ff469ff8
kenning <hwerzog@gmail.com>
2018-07-21 11:52:25 -0700

    Character controller working

---
+++ Shadow/Assets/Config.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Singleton class that allows us to set constants in the inspector.
+/// </summary>
+public class Config : MonoBehaviour {
+	// It's a singleton
+	static Config config;
+	void Awake() {
+    config = this;
+  }
+
+  // Bunch of publicly accessible variables in the singleton style
+	[SerializeField]
+	float movementSpeed;
+	public static float MovementSpeed { get { return config.movementSpeed; } }
+
+	[SerializeField]
+	int numberOfPlayers;
+	public static int NumberOfPlayers { get { return config.numberOfPlayers; } }
+
+}
---
+++ Shadow/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	public bool IsShadow;
+	public int PlayerNumber; // 1-indexed
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerNumber < 1 || PlayerNumber > Config.NumberOfPlayers) {
+			Debug.LogError(
+				"The max number of players is " + 
+				Config.NumberOfPlayers + 
+        " and player numbers start at 1"
+			);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var xVelocity = Input.GetAxis("HorizontalPlayer" + PlayerNumber);
+		var yVelocity = Input.GetAxis("VerticalPlayer" + PlayerNumber);
+
+		gameObject.GetComponent<Rigidbody>().velocity = new Vector3(
+			xVelocity * Config.MovementSpeed,
+      0,
+			yVelocity * Config.MovementSpeed
+		);
+
+		Vector3 direction = (Vector3.right * xVelocity) + (Vector3.forward * yVelocity);
+		if (direction != Vector3.zero) {
+      transform.rotation=Quaternion.LookRotation(direction);
+		}
+	}
+}
