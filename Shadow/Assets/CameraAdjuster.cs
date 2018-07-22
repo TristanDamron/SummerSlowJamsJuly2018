67fac7c209e311bcff5bb6dc905335c21a63eddd
kenning <hwerzog@gmail.com>
2018-07-22 15:53:03 -0700

    vhs shader

--- Shadow/Assets/CameraAdjuster.cs
+++ Shadow/Assets/CameraAdjuster.cs
@@ -8,6 +8,9 @@ using System.Linq;
 /// </summary>
 public class CameraAdjuster : MonoBehaviour {
 	static Camera cam;
+	static TV_Effect tvEffect;
+	static VHS_Effect vhsEffect;
+
 	static CameraAdjuster cameraAdjuster;
 	static bool initialized;
 	static List<Transform> playerTransforms;
@@ -25,6 +28,8 @@ public class CameraAdjuster : MonoBehaviour {
 		cameraAdjuster = this;
 		playerTransforms = new List<Transform>();
 		cam = gameObject.GetComponent<Camera>();
+		tvEffect = gameObject.GetComponent<TV_Effect>();
+		vhsEffect = gameObject.GetComponent<VHS_Effect>();
 	}
 
 	void Update() {
@@ -47,7 +52,10 @@ public class CameraAdjuster : MonoBehaviour {
 			float xMaxDifference = (furthestEast - furthestWest) / maxXPosition;
 			float zMaxDifference = (furthestNorth - furthestSouth) / maxZPosition;
 			float largerDifference = xMaxDifference > zMaxDifference ? xMaxDifference : zMaxDifference;
+			largerDifference = Mathf.Min(1f, largerDifference);
 			targetCameraHeight = (largerDifference * targetCameraHeight) + 40;
+
+			DoVhsShaders(1 - largerDifference);
 
 			cameraTargetPosition = new Vector3(
 				(furthestEast + furthestWest) / 2,
@@ -63,10 +71,34 @@ public class CameraAdjuster : MonoBehaviour {
 		if (!initialized) {
       GameObject.Find("Ready Up Text").SetActive(false);
 			playerTransforms = GameObject.FindGameObjectsWithTag("Player")
-			                             .Select(x => { print(x.name); return x.transform; })
+        .Select(x => x.transform)
         .ToList();
 			playerTransforms.Add(GameObject.FindGameObjectWithTag("Shadow").transform);
 		}
+	}
+
+  // Max is 1f, min is ~0.07f
+	void DoVhsShaders(float magnitude) {
+		// Square effect so it's way more pronounced when shadow is nearby
+		magnitude = magnitude * magnitude;
+
+    // Scanlines. The inspector slider goes from -8f to -16f but it gets set to 0 by default
+    // -16 is basically impossible to see, but 0f makes everything too bright
+    // Maybe take this out
+		//tvEffect._hardScan = -4f * magnitude * 4f;
+		//tvEffect._resolution = (magnitude + 1f);
+
+    // 3d glasses style color offset
+		// 0.005 = almost none, 0.0128 = extreme amount
+		vhsEffect.offsetColor = magnitude * .01f;
+
+    // Camera jumping.
+    // 0 = none, 0.05 = extreme
+		vhsEffect._verticalOffset = magnitude * .012f;
+
+    // Overlaid "grainy" texture
+    // 1 = none, .9 = very noticeable, .8 = way too intrusive
+		vhsEffect._textureIntensity = .95f + -(magnitude) * .1f;
 	}
 }
 
