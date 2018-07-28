4ddcd09f785fbb330abbadd0ee43adb175c45b7e
Tristan Damron <TristanDamron31@gmail.com>
2018-07-28 13:27:03 -0700

    Added player profiles.

--- Shadow/Assets/PlayerSpawn.cs
+++ Shadow/Assets/PlayerSpawn.cs
@@ -11,6 +11,8 @@ public class PlayerSpawn : MonoBehaviour {
 	private GameObject _shadow;
     [SerializeField]
     private GameObject _healthBar;
+	[SerializeField]
+	private RenderTexture _texture;
 
 	void Update () {
 		if (Input.GetAxis("ShootPlayer" + _playerNum) != 0f && !Config.Paused) {
@@ -20,11 +22,14 @@ public class PlayerSpawn : MonoBehaviour {
 			if (Config.ReadyPlayers >= Config.NumberOfPlayers) {
 				var p = Instantiate(_shadow, transform.position, transform.rotation);
 				p.GetComponent<PlayerController>().PlayerNumber = _playerNum;
+				p.transform.Find("Profile Camera").GetComponent<Camera>().targetTexture = _texture;				
 				Config.StartGame();
 			} else {
 				var p = Instantiate(_player, transform.position, transform.rotation);
 				p.GetComponent<PlayerController>().PlayerNumber = _playerNum;
+				p.transform.Find("Profile Camera").GetComponent<Camera>().targetTexture = _texture;
 			}
+
             _healthBar.SetActive(true);
 
 			Destroy(gameObject);
