561d092f1596f171fd9698d643dc4760b4ddfabe
Tristan Damron <TristanDamron31@gmail.com>
2018-07-28 12:18:36 -0700

    Added a basic restart button.

---
+++ Shadow/Assets/RestartButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour {
+	public void Restart() {
+		SoundManager.Inst.PlaySound(SoundManager.Inst.ButtonPressClip);				
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
