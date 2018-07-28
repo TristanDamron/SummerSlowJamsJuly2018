cd711e5832d3d13b78456df86ffa0a76c451a327
Tristan Damron <TristanDamron31@gmail.com>
2018-07-27 19:37:27 -0700

    Added countdown timer.

---
+++ Shadow/Assets/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour {
+	[SerializeField]
+	private float _timer;
+	[SerializeField]
+	private TMP_Text _timerText;
+	[SerializeField]
+	private float _delay;
+	private int _hours;
+	private int _minutes; 
+	
+	void Update () {
+		if (!Config.Paused) {
+			_timer += Time.deltaTime;
+			if (_timer > _delay) {
+				_minutes++;
+				if (_minutes >= 60f) {
+					_hours++;
+					_minutes = 0;
+				}
+
+				var strHours = _hours.ToString();
+				var strMinutes = _minutes.ToString();
+
+				if (strHours.Length < 2) {
+					strHours = "0" + _hours.ToString();
+				} 
+				
+				if (strMinutes.Length < 2) {
+					strMinutes = "0" + _minutes.ToString();
+				}
+
+				_timerText.text = strHours + ":" + strMinutes;
+				_timer = 0f;
+			}
+		}
+	}
+}
