284e9e1e2de04d67f81b98dcca13cca44f936a43
Tristan Damron <TristanDamron31@gmail.com>
2018-07-29 10:11:29 -0700

    Added instruction screen placeholder.

--- Shadow/Assets/StartButton.cs
+++ Shadow/Assets/StartButton.cs
@@ -9,6 +9,7 @@ public class StartButton : MonoBehaviour {
 	private RawImage _image;
 	[SerializeField]
 	private VideoClip _active;
+	private bool _fall;
 
 	void Start () {
 		Config.Paused = true;
@@ -22,9 +23,19 @@ public class StartButton : MonoBehaviour {
 
 	IEnumerator CleanUp() {
 		yield return new WaitForSeconds(5f);		
-		Config.Paused = false;
 		_image.gameObject.SetActive(false);
 		gameObject.SetActive(false);
+	}
 
+	public void ReadyButton() {
+		SoundManager.Inst.PlaySound(SoundManager.Inst.ButtonPressClip);		
+		Config.Paused = false;		
+		_fall = true;
+	}
+
+	void Update() {
+		if (_fall) {
+			transform.parent.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -800, transform.position.z), Time.deltaTime);
+		}
 	}
 }
