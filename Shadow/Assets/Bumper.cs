e637d618c6296a39dbfd7c8493059ba283762621
Tristan Damron <TristanDamron31@gmail.com>
2018-07-29 09:25:38 -0700

    Added bumper.

---
+++ Shadow/Assets/Bumper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Bumper : MonoBehaviour {
+	private VideoPlayer _player;
+	private float _playbackTime;		
+	[SerializeField]
+	private GameObject _next;
+
+	void Start() {
+		_player = GetComponent<VideoPlayer>();
+	}
+
+	void Update () {
+		_playbackTime += Time.deltaTime;			
+		if (_playbackTime >= _player.clip.length) {
+			_next.SetActive(true);
+			gameObject.SetActive(false);
+		}		
+	}
+}
